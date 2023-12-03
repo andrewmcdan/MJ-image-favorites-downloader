09a063b9fe14f15d0b90949630617da56f54e5ef
Andrew <andrewmcdan@gmail.com>
2023-12-02 20:58:26 -0500

    update show options

--- app.js
+++ app.js
@@ -1868,7 +1868,7 @@ app.get('/saveSettings', async (req, res) => {
 });
 
 app.get('/showOptions', async (req, res) => {
-    res.json({ enableAutoAdjustUpdateInterval: false, updateInterval: 3, fadeDuration: 2, timeToRestart: 0, timeToRestartEnabled: true, showPrompt: false });
+    res.json({ enableAutoAdjustUpdateInterval: false, updateInterval: 12, fadeDuration: 3.5, timeToRestart: 60, timeToRestartEnabled: true, showPrompt: false });
 });
 
 ////////////////////////////////////////////////////////////////////////////////////////
