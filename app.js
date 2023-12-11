5414a05ca6960436fa62e0800edfe3bee1d64808
Andrew <andrewmcdan@gmail.com>
2023-12-10 19:23:01 -0500

    ...

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ const winstonLogger = winston.createLogger({
         winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
     ),
     transports: [
-        new winston.transports.Console(),
+        // new winston.transports.Console(),
         logFileTransport,
         dbTransport
     ]
